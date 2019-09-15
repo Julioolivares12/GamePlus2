e1823ebbe1e30285dd59b410510cfa672d2037ee
mcortez07 <2520492015@mail.utec.edu.sv>
2019-09-15 17:37:19 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/julio/gameplus2/ui/home/HomeFragment.kt
    #	app/src/main/res/layout/fragment_home.xml

--- app/src/main/java/com/julio/gameplus2/ui/peores/PeoresFragment.java
+++ app/src/main/java/com/julio/gameplus2/ui/peores/PeoresFragment.java
@@ -6,14 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.julio.gameplus2.R;
+import com.julio.gameplus2.ui.Adaptadores.AdapterPeores;
+import com.julio.gameplus2.ui.Adaptadores.AdapterTelefonos;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PeoresFragment extends Fragment {
+    GridView item;
+    String []titulo;
+    int[]imagen = {R.drawable.peor1,
+            R.drawable.peor2,
+            R.drawable.peor3
 
+    };
 
     public PeoresFragment() {
         // Required empty public constructor
@@ -23,8 +32,13 @@ public class PeoresFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_peores, container, false);
+        View view = inflater.inflate(R.layout.fragment_peores, container, false);;
+        titulo = getResources().getStringArray(R.array.titulos);
+        AdapterPeores adapte = new AdapterPeores(getContext(), titulo, imagen);
+        item = (GridView) view.findViewById(R.id.grid_peores);
+        item.setAdapter(adapte);
+
+        return view;
     }
 
 }
