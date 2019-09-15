c1bd7a8ee5f632082fe08ba714113495d229c5e9
mcortez07 <2520492015@mail.utec.edu.sv>
2019-09-15 16:51:08 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/julio/gameplus2/ui/home/HomeFragment.kt
    #	app/src/main/res/layout/fragment_home.xml

--- app/src/main/java/com/julio/gameplus2/ui/contacto/ContactoFragment.java
+++ app/src/main/java/com/julio/gameplus2/ui/contacto/ContactoFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.julio.gameplus2.R;
 
@@ -13,7 +14,10 @@ import com.julio.gameplus2.R;
  * A simple {@link Fragment} subclass.
  */
 public class ContactoFragment extends Fragment {
-
+    GridView item;
+    String []sucursal;
+    String []telefono;
+    int imagen1;
 
     public ContactoFragment() {
         // Required empty public constructor
@@ -23,6 +27,7 @@ public class ContactoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contacto, container, false);
     }
