05ab0d5ab63b01f4bdfff8ad602f56b43e0cede7
julio <julioolivares90@gmail.com>
2019-09-15 16:17:35 -0600

    videojuegos clase

---
+++ app/src/main/java/com/julio/gameplus2/adapters/MasEsperadosAdapter.java
@@ -0,0 +1,67 @@
+package com.julio.gameplus2.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.julio.gameplus2.R;
+import com.julio.gameplus2.listeners.OnItemClickListener;
+import com.julio.gameplus2.modelos.VideoJuego;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class MasEsperadosAdapter extends RecyclerView.Adapter<MasEsperadosAdapter.MasEsperadosHolder> {
+    private final OnItemClickListener onItemClickListener;
+    List<VideoJuego>videoJuegos;
+    public MasEsperadosAdapter(List<VideoJuego> videoJuegos,OnItemClickListener onItemClickListener){
+        this.videoJuegos = videoJuegos;
+        this.onItemClickListener = onItemClickListener;
+
+    }
+    @NonNull
+    @Override
+    public MasEsperadosHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_mas_esperados,viewGroup,false);
+        return new MasEsperadosHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MasEsperadosHolder masEsperadosHolder, int i) {
+        VideoJuego videoJuego = videoJuegos.get(i);
+        masEsperadosHolder.bind(videoJuego,onItemClickListener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return videoJuegos.size();
+    }
+
+    class MasEsperadosHolder extends RecyclerView.ViewHolder{
+
+        private ImageView iv_masEsperado;
+        private TextView tv_title_masEsperado;
+        public MasEsperadosHolder(@NonNull View itemView) {
+            super(itemView);
+            iv_masEsperado = itemView.findViewById(R.id.iv_masEsperados);
+            tv_title_masEsperado = itemView.findViewById(R.id.tv_title_masEsperado);
+        }
+
+        public void bind(final VideoJuego videoJuego, final OnItemClickListener onItemClickListener){
+            iv_masEsperado.setImageResource(videoJuego.getImagen());
+            tv_title_masEsperado.setText(videoJuego.getNombre());
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onClick(videoJuego);
+                }
+            });
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/julio/gameplus2/modelos/VideoJuego.java
@@ -0,0 +1,38 @@
+package com.julio.gameplus2.modelos;
+
+public class VideoJuego {
+    private String nombre;
+    private String descripcion;
+
+    public VideoJuego(String nombre, String descripcion, int imagen) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+
+    private int imagen;
+}
