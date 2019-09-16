478d03c320f15f7b9d1cfd1009c8a71b83bc7d7e
mcortez07 <2520492015@mail.utec.edu.sv>
2019-09-15 19:41:47 -0600

    cambio por

--- app/src/main/java/com/julio/gameplus2/ui/peores/DetallePeoresFragment.java
+++ app/src/main/java/com/julio/gameplus2/ui/peores/DetallePeoresFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.julio.gameplus2.R;
 
@@ -20,8 +22,24 @@ public class DetallePeoresFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalle_peores, container, false);
+        View view = inflater.inflate(R.layout.fragment_detalle_peores, container, false);
+        TextView txtTitulo = (TextView)view.findViewById(R.id.tvDetalleTitulo);
+        TextView txtComentario = (TextView)view.findViewById(R.id.tvComentario);
+        ImageView imagen = (ImageView)view.findViewById(R.id.imgDetallePeor);
+        String titulo,comentario;
+        int img;
+
+        Bundle b = getArguments();
+        titulo = b.getString("titulo","");
+        comentario = b.getString("comentario","");
+        img = b.getInt("imagen",0);
+        txtTitulo.setText(titulo);
+        txtComentario.setText(comentario);
+        imagen.setImageResource(img);
+
+
+
+        return view;
     }
 
 }
